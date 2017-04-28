24ed838a1faabf3d564a9c9c7803282b5feb2a99
James Kominick <james.kominick@gmail.com>
2017-04-27 20:49:05 -0400

    update schema

--- server/migrations/20170426011626_initial/up.sql
+++ server/migrations/20170426011626_initial/up.sql
@@ -2,9 +2,17 @@ create table items (
     id              serial PRIMARY KEY,
     key             text UNIQUE NOT NULL,
     filepath        text NOT NULL,
-    secret          text,
     lifespan        bigint,
+    dl_limit        integer,
     accesses        integer NOT NULL DEFAULT 0,
     date_created    timestamp WITH TIME ZONE NOT NULL DEFAULT NOW(),
     date_viewed     timestamp WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+create table creds (
+    id              serial PRIMARY KEY,
+    access_key      bytea,
+    salt            bytea,
+    item_id         integer NOT NULL UNIQUE REFERENCES "items" ("id") ON DELETE CASCADE,
+    date_created    timestamp WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
