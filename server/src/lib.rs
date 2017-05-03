f41661d84cac18fbcc956ccef8f34d58fadceca1
James Kominick <james.kominick@gmail.com>
2017-05-02 23:26:49 -0400

    update deps/crate info

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -2,14 +2,23 @@
 #[macro_use] extern crate error_chain;
 
 extern crate dotenv;
+extern crate chrono;
+extern crate rand;
 
-extern crate diesel;
+#[macro_use] extern crate diesel;
+#[macro_use] extern crate diesel_codegen;
 extern crate r2d2;
 extern crate r2d2_diesel;
+
+extern crate serde;
+#[macro_use] extern crate serde_derive;
+#[macro_use] extern crate serde_json;
+
 
 #[macro_use] extern crate tera;
 
 #[macro_use] extern crate mime;
+extern crate params;
 extern crate env_logger;
 
 extern crate iron;
@@ -26,3 +35,5 @@ pub mod errors {
 pub mod service;
 pub mod handlers;
 pub mod routes;
+pub mod schema;
+pub mod models;
